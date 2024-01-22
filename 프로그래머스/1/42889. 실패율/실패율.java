36d2069c523e24daa4e27d9b19019409b9c84e89
이채은 <109871579+ChaeAg@users.noreply.github.com>
2024-01-23 05:32:03 +0900

    [level 1] Title: 실패율, Time: 10.08 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        Arrays.sort(stages);
+        Map<Integer, Float> failRate = new LinkedHashMap<>();
+        for(int i=1; i<=N; i++) {
+            float same = 0.0f;
+            float dodal = 0.0f;
+            for(int j=0; j<stages.length; j++) {
+                if(stages[j] == i) {
+                    dodal++;
+                    same++;
+                }
+                else if(stages[j] > i) dodal++;
+            }
+            if(same != 0 || dodal != 0) failRate.put(i, same/dodal);
+            else failRate.put(i, 0.0f);
+        }
+        List<Integer> keys = new ArrayList<>(failRate.keySet());
+        for(int key : keys) {
+            System.out.println(key + " " + failRate.get(key));
+        }
+		Collections.sort(keys, (v1, v2) -> (failRate.get(v2).compareTo(failRate.get(v1))));
+        int idx = 0;
+        for(int key : keys) {
+            answer[idx++] = key;
+        }
+        for(int i=1; i<=N; i++) {
+            if(!keys.contains(i)) {
+                answer[idx++] = i;
+            }
+        }
+        
+        return answer;
+    }
+}
