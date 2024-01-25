9b00078d42e28daa5530ffe9c3f5969434b045e7
이채은 <109871579+ChaeAg@users.noreply.github.com>
2024-01-26 07:44:47 +0900

    [Silver V] Title: 방 번호, Time: 128 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -18,23 +18,24 @@ public class Main {
         numCounts.put(3, N.length() - N.replaceAll("3", "").length());
         numCounts.put(4, N.length() - N.replaceAll("4", "").length());
         numCounts.put(5, N.length() - N.replaceAll("5", "").length());
-
-        // 6과 9를 하나로 생각하긔
+        // 6과 9를 하나로 생각한다.
         numCounts.put(6, N.length() - N.replaceAll("6", "").replaceAll("9","").length());
-
         numCounts.put(7, N.length() - N.replaceAll("7", "").length());
         numCounts.put(8, N.length() - N.replaceAll("8", "").length());
 
         int maxCountNum = 0;
-        int sixAndNineCalculate = numCounts.get(6) % 2 == 0 ? numCounts.get(6) / 2 : numCounts.get(6) / 2 + 1;
+        int sixAndNineCalculate = numCounts.get(6) % 2 == 0 ? numCounts.get(6) / 2 : numCounts.get(6) / 2 + 1; // 6과 9가 서로 대체되었을 때 사야하는 최소한의 개수
 
         for(int key : numCounts.keySet()) {
+            // 6(9)를 2로 나눠도 제일 많이 등장했을 경우
             if(key == 6 && numCounts.get(maxCountNum) < sixAndNineCalculate) {
                 maxCountNum = 6;
             }
+            // 6(9)등장횟수 / 2(최다등장) 보다 더 많이 등장했을 경우
             else if(maxCountNum == 6 && sixAndNineCalculate < numCounts.get(key)) {
                 maxCountNum = key;
             }
+            // 그냥 많이 등장했을 경우
             else if(maxCountNum != 6 && numCounts.get(maxCountNum) < numCounts.get(key)) {
                 maxCountNum = key;
             }
