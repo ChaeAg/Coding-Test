719a40cab6aaf97e170e374898cb2fa243dd3a22
이채은 <109871579+ChaeAg@users.noreply.github.com>
2024-03-17 17:16:21 +0900

    [Silver II] Title: 섬의 개수, Time: 180 ms, Memory: 18212 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -14,6 +14,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         while (true) {
+            /*
+            변수 세팅
+            */
             st = new StringTokenizer(br.readLine());
             w = Integer.parseInt(st.nextToken());
             h = Integer.parseInt(st.nextToken());
@@ -24,8 +27,9 @@ public class Main {
             }
             check = new boolean[h][w];
             int island_count = 0;
-
-            // dfs 고고
+            /*
+            dfs 고고
+             */
             for(int i=0; i<h; i++) {
                 for(int j=0; j<w; j++) {
                     if(dfs(i, j, false)) island_count++;
@@ -42,18 +46,21 @@ public class Main {
         }
 
         if(!check[x][y] && map[x][y].equals("1")) {
+            // 방문한 1은 이미 센 섬이니 true로 바꿔주고 다시 false로 바꿔줄 필요가 없다.
             check[x][y] = true;
+            // 처음 들어온 섬 일부분(사각형)이면 isIsland가 false니 true로 바꿔준다.
             if(!isIsland) {
                 isIsland = true;
             }
             // 상하좌우대각선 모두 탐색
             for(int i=0; i<4; i++) {
-                for (int j = 0; j < 4; j++) {
+                for (int j=0; j<4; j++) {
                     dfs(x + move_h[i], y + move_w[j], isIsland);
                 }
             }
         }
 
+        // 섬이 있었는지 반환
         return isIsland;
     }
 }
