600f403784e57b7d4ba890b46b40d2890e8c14b4
이채은 <109871579+ChaeAg@users.noreply.github.com>
2024-04-06 18:13:11 +0900

    [level 2] Title: 기능개발, Time: 0.38 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -6,31 +6,29 @@ class Solution {
         int before_day = 0;
         int idx = 0;
         Map<Integer, Integer> map = new LinkedHashMap<>();
-        
+
         for(int progress : progresses) { // 큐 세팅
             q.offer(progress);
         }
-        
+
         while(!q.isEmpty()) {
             int prog = q.poll(); // 먼저 개발해야 하는 기능 순으로 나옴
-            
-            while(prog + (speeds[idx] * day) < 100) {
-                day++;
+
+            day = (int) Math.ceil((100 - prog) / (double)speeds[idx]); // 이 기능을 개발하는데 걸리는 기간
+
+            if(before_day >= day) { // 이미 개발 되어 있는 기능일 경우
+                map.replace(before_day, map.get(before_day) + 1);
             }
-            
-            if(before_day == day) { // 위 while을 거치지 않고 이미 개발 된 기능일 경우
-                map.replace(day, map.get(day) + 1);
-            }
-            else {
+            else { // 기간을 더 들여서 개발해야 할 경우
                 map.put(day, 1);
             }
-            
+
             idx++;
-            before_day = day;
+            before_day = before_day < day ? day : before_day;
         }
-        
+
         int[] answer = new int[map.size()];
-        int i = 0; 
+        int i = 0;
         for(int cnt : map.values()) {
             answer[i++] = cnt;
         }
