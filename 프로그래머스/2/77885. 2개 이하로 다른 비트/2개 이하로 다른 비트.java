2a25507120b3c8d19d8de099fb1d5a8e08693cc1
이채은 <109871579+ChaeAg@users.noreply.github.com>
2024-08-28 18:45:05 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 687.71 ms, Memory: 402 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long[] solution(long[] numbers) {
+        long[] answer = new long[numbers.length];
+
+        for(int i=0; i<numbers.length; i++) {
+            String[] strs = Long.toBinaryString(numbers[i]).split("");
+            String result = "";
+            for(int j=strs.length-1; j>=0; j--) {
+                if(strs[j].equals("0")) {
+                    strs[j] = "1";
+                    if(j < strs.length-1) strs[j+1] = "0";
+                    result = String.join("", strs);
+                    break;
+                }
+
+                if(j == 0) { // 2진수의 모든 비트가 1일 경우에 여기 도달
+                    strs[j] = "0";
+                    result = "1" + String.join("", strs);
+                }
+            }
+            answer[i] = Long.parseLong(result, 2);
+        }
+        
+        return answer;
+    }
+}
