ade28634f0f12d4b183315ccbc062f43a26ffb35
이채은 <109871579+ChaeAg@users.noreply.github.com>
2024-11-22 21:10:39 +0900

    [Silver V] Title: 올림픽, Time: 152 ms, Memory: 15960 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -23,23 +23,24 @@ public class Main {
                 if(o1[2] == o2[2]) {
                     return o2[3] - o1[3];
                 }
-                return o2[1] - o1[2];
+                return o2[2] - o1[2];
             }
             return o2[1] - o1[1];
         });
 
-        int grade = 0;
-        int gauge = 1;
+        int rank = 0, gauge = 1;
         for(int i=0; i<N; i++) {
             if(i == N-1) {
-                grade += gauge;
+                if(scores[i][1] != scores[i-1][1] || scores[i][2] != scores[i-1][2] || scores[i][3] != scores[i-1][3]) {
+                    rank += gauge;
+                }
                 break;
             }
             if(i != 0 && scores[i][1] == scores[i-1][1] && scores[i][2] == scores[i-1][2] && scores[i][3] == scores[i-1][3]) {
                 gauge++;
             }
             else {
-                grade += gauge;
+                rank += gauge;
                 gauge = 1;
             }
             if(scores[i][0] == K) {
@@ -47,6 +48,6 @@ public class Main {
             }
         }
 
-        System.out.print(grade);
+        System.out.print(rank);
     }
 }
