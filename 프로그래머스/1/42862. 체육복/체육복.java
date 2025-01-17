5df17f1caaec5fde28b2ee16822edcf17116d3be
이채은 <109871579+ChaeAg@users.noreply.github.com>
2025-01-17 16:20:34 +0900

    [level 1] Title: 체육복, Time: 6.47 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -3,47 +3,36 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        List<Integer> reserveList = Arrays.stream(reserve)
+        int answer = n - lost.length;
+        
+        List<Integer> lo = Arrays.stream(lost)
             .boxed()
-            .collect(Collectors.toList());
-        List<Integer> lostList =  Arrays.stream(lost)
-            .boxed()
-            .collect(Collectors.toList());
-        List<Integer> have = new ArrayList();
-        for(int i=1; i<=n; i++) {
-            have.add(i);
-        }
-        for(int lostStudent : lost) {
-            if(have.contains(lostStudent)) have.remove(have.indexOf(lostStudent));
-        }
-        for(int reserveStudent : reserve) {
-            if(have.contains(reserveStudent)) have.remove(have.indexOf(reserveStudent));
-        }
-        int haveCount = have.size();
-        for(int i=0; i<reserve.length; i++) { // 여벌 체육복을 가져온 학생이 도난당한 경우를 카운트
-            haveCount += 1;
-            if(lostList.contains(reserve[i])) {
-                lostList.remove(lostList.indexOf(reserve[i]));
-                reserveList.remove(reserveList.indexOf(reserve[i]));
-            }
-        }
-        int[] lostArr = lostList.stream()
             .sorted()
-            .mapToInt(Integer::intValue)
-            .toArray();
+            .collect(Collectors.toList());
+        List<Integer> re = Arrays.stream(reserve)
+            .boxed()
+            .sorted()
+            .collect(Collectors.toList());
         
-        for(int i=0; i<lostArr.length; i++) {
-            if(reserveList.contains(lostArr[i]-1)) {
-                haveCount += 1;
-                reserveList.remove(reserveList.indexOf(lostArr[i]-1));
-                continue;
-            }
-            if(reserveList.contains(lostArr[i]+1)) {
-                haveCount += 1;
-                reserveList.remove(reserveList.indexOf(lostArr[i]+1));
+        for(int i : lost) {
+            if(re.contains(i)) {
+                answer++;
+                lo.remove(lo.indexOf(i));
+                re.remove(re.indexOf(i));
             }
         }
         
-        return haveCount;
+        for(int i : lo) {
+            if(re.contains(i-1)) {
+                answer++;
+                re.remove(re.indexOf(i-1));
+            }
+            else if(re.contains(i+1)) {
+                answer++;
+                re.remove(re.indexOf(i+1));
+            }
+        }
+        
+        return answer;
     }
 }
