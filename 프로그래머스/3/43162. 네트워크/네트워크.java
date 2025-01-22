337d2b25802d6a106725bad4103294da78d1c92b
이채은 <109871579+ChaeAg@users.noreply.github.com>
2025-01-22 12:01:17 +0900

    [level 3] Title: 네트워크, Time: 0.36 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,66 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     int n;
+//     int[][] computers;
+//     boolean[] visited;
+//     public int solution(int n, int[][] computers) {
+//         int answer = 0;
+//         this.n = n;
+//         this.computers = computers;
+//         visited = new boolean[n];
+
+//         for(int i=0; i<n; i++) {
+//             if(!visited[i]) {
+//                 dfs(i);
+//                 answer++;
+//             }
+//         }
+        
+//         return answer;
+//     }
+    
+//     public void dfs(int num) {
+//         visited[num] = true;
+        
+//         for(int i=0; i<n; i++) {
+//             if(num != i && !visited[i] && computers[num][i] == 1) {
+//                 visited[i] = true;
+//                 dfs(i);
+//             }
+//         }
+//     }
+// }
 
 class Solution {
-    int n;
-    int[][] computers;
-    boolean[] visited;
+    static int copy_n;
+    static int[][] copy_computers;
+    static boolean[] visited;
+
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        this.n = n;
-        this.computers = computers;
+        copy_n = n;
+        copy_computers = computers;
         visited = new boolean[n];
 
-        for(int i=0; i<n; i++) {
-            if(!visited[i]) {
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
                 dfs(i);
                 answer++;
             }
         }
-        
+
         return answer;
     }
-    
-    public void dfs(int num) {
-        visited[num] = true;
-        
-        for(int i=0; i<n; i++) {
-            if(num != i && !visited[i] && computers[num][i] == 1) {
-                visited[i] = true;
-                dfs(i);
+
+    public static void dfs(int i) {
+        for (int k = 0; k < copy_n; k++) {
+            if (i == k || copy_computers[i][k] != 1 || visited[k]) {
+                continue;
             }
+            visited[k] = true; // 방문했다고 체크
+            dfs(k);
         }
     }
 }
