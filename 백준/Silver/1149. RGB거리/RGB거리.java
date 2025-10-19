8f957ea9238bf912361a79a0046117ff6d489df0
ChaeAg <109871579+ChaeAg@users.noreply.github.com>
2025-10-19 21:56:47 +0900

    [Silver I] Title: RGB거리, Time: 108 ms, Memory: 14484 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    static int[][] house;
+    static int N;
+    static int min_sum = 1_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        house = new int[N][3];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                house[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] dp = new int[N][3];
+        dp[0] = house[0];
+
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = house[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+            dp[i][1] = house[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
+            dp[i][2] = house[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+        }
+
+        System.out.print(Math.min(dp[N - 1][2], Math.min(dp[N - 1][0], dp[N - 1][1])));
+    }
+}
