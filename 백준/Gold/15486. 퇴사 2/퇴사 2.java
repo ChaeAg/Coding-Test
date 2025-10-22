9741bc670e356ac0d377732b903bdfb63e3b55c9
ChaeAg <109871579+ChaeAg@users.noreply.github.com>
2025-10-22 15:22:25 +0900

    [Gold V] Title: 퇴사 2, Time: 864 ms, Memory: 340476 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -15,29 +15,19 @@ class Main {
             arr[i][1] = Integer.parseInt(tmp[1]);
         }
 
-        int max = 0;
         int[] dp = new int[N + 2];
 
         for (int i = 1; i <= N; i++) {
-            int next_m = arr[i][1];
+            dp[i + 1] = Math.max(dp[i], dp[i + 1]);
 
-            if (dp[i] != 0) { // 더해서 시작할 수 있다는 뜻
-                next_m += dp[i];
-//                for (int d = i + 1; d < Math.min(arr[i][0] + i, N + 1); d++) {
-//                    dp[d] = Math.max(dp[d], dp[i]);
-//                }
-
-                dp[i + 1] = Math.max(dp[i], dp[i + 1]);
-            }
-
+            int next_m = arr[i][1] + dp[i];
             int next_d = i + arr[i][0];
 
             if (next_d <= N + 1 && dp[next_d] < next_m) { // 이득이라서 덮어씌우기
                 dp[next_d] = next_m;
-                max = Math.max(dp[next_d], max);
             }
         }
 
-        System.out.print(max);
+        System.out.print(dp[N + 1]);
     }
 }
