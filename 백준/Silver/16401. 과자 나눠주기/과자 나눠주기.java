8f5f68b5aec5b2357f2ea983587c5ca76f19ce71
ChaeAg <109871579+ChaeAg@users.noreply.github.com>
2025-10-24 17:39:20 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1132 ms, Memory: 124176 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int left = 0, right = arr[N - 1];
+        int answer = 0;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (mid == 0) {
+                mid = 1;
+                left++;
+            }
+            int cnt = 0;
+
+            for (int i : arr) {
+                cnt += i / mid;
+            }
+
+            if (cnt >= M) {
+                answer = Math.max(answer, mid);
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
