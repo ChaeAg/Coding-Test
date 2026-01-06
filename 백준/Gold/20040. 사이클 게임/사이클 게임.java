789c11b89df65111f6522f369ba941034348d6e3
ChaeAg <109871579+ChaeAg@users.noreply.github.com>
2026-01-06 15:39:20 +0900

    [Gold IV] Title: 사이클 게임, Time: 524 ms, Memory: 151236 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+
+            int root1 = find(node1);
+            int root2 = find(node2);
+
+            if (root1 == root2) {
+                System.out.print(i + 1);
+                return;
+            }
+
+            union(root1, root2);
+        }
+
+        System.out.print(0);
+    }
+
+    static int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    static void union(int x, int y) {
+        parent[x] = y;
+    }
+}
