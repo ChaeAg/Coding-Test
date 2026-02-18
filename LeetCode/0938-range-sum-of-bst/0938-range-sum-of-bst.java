bb261b23db26cae386cabff56342eac397e400e4
ChaeAg <109871579+ChaeAg@users.noreply.github.com>
2026-02-18 09:54:58 +0900

    [Easy] Title: 0938-range-sum-of-bst, Time: 1 ms (43.46%), Memory: 54.1 MB (25.88%) - LeetHub

---
+++ LeetCode/0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution { // 전위순회
+    int low, high;
+    boolean isReal = false;
+    int sum = 0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        this.low = low;
+        this.high = high;
+        traversal(root);
+        return sum;
+    }
+
+    public void traversal(TreeNode root) {
+        if(root == null) return;
+
+        traversal(root.left); // 왼쪽 노드로
+
+        if(root.val >= low && root.val <= high) { // 본인 노드
+            sum += root.val;
+            if(isReal && root.val == high) return;
+            if(root.val == low) isReal = true;
+        }
+
+        traversal(root.right); // 오른쪽 노드로
+    }
+}
