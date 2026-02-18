7611a1ba827eda81700c934a1d1086df13c21f20
ChaeAg <109871579+ChaeAg@users.noreply.github.com>
2026-02-18 22:53:38 +0900

    [Easy] Title: 0953-verifying-an-alien-dictionary, Time: 5 ms (2.48%), Memory: 43.6 MB (49.53%) - LeetHub

--- LeetCode/0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
+++ LeetCode/0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
@@ -3,25 +3,26 @@ class Solution {
     public boolean isAlienSorted(String[] words, String order) {
         int len = words.length;
         String[] backupWords = Arrays.copyOf(words, len);
-        List<String> orderList = List.of(order.split(""));
-
-        String[][] wordsArr = new String[len][20];
-        for (int i = 0; i < len; i++) {
-            wordsArr[i] = words[i].split("");
+        Map<Character, Integer> orderMap = new HashMap<>();
+        int rank = 1;
+        for(char o : order.toCharArray()) {
+            orderMap.put(o, rank++);
         }
 
-        Arrays.sort(wordsArr, (o1, o2) -> {
-            int minLen = Math.min(o1.length, o2.length);
+        Arrays.sort(words, (o1, o2) -> {
+            int minLen = Math.min(o1.length(), o2.length());
             for (int i = 0; i < minLen; i++) {
-                if (!o1[i].equals(o2[i])) {
-                    return orderList.indexOf(o1[i]) - orderList.indexOf(o2[i]);
+                char c1 = o1.charAt(i);
+                char c2 = o2.charAt(i);
+                if (c1 != c2) {
+                    return orderMap.get(c1) - orderMap.get(c2);
                 }
             }
-            return o1.length - o2.length;
+            return o1.length() - o2.length();
         });
 
         for (int i = 0; i < len; i++) {
-            if (!backupWords[i].equals(String.join("", wordsArr[i]))) {
+            if (!backupWords[i].equals(words[i])) {
                 return false;
             }
         }
