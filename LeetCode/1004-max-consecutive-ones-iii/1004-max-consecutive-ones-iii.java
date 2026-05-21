b58bc55d3446635b6bcf506fbd48fa9216437a60
ChaeAg <109871579+ChaeAg@users.noreply.github.com>
2026-05-21 10:52:59 +0900

    [Medium] Title: 1004-max-consecutive-ones-iii, Time: 13 ms (6.99%), Memory: 53.7 MB (7.07%) - LeetHub

---
+++ LeetCode/1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int total_len = 0;
+        int max = 0;
+        int used = 0;
+        Queue<Integer> que = new LinkedList<>();
+        for(int num : nums) {
+            if(k == 0) {
+                if(num == 1) {
+                    total_len++;
+                } else {
+                    total_len = 0;
+                }
+                max = Math.max(max, total_len);
+                continue;
+            }
+            if(num == 0) {
+                if(used == k) {
+                    while(!que.isEmpty()) {
+                        int i = que.poll();
+                        total_len--;
+                        if(i == 1) {
+                            continue;
+                        } else {
+                            used--;
+                            break;
+                        }
+                    }
+                }
+                used++;
+            }
+            que.add(num);
+            total_len++;
+            max = Math.max(max, total_len);
+        }
+
+        return max;
+    }
+}
