ec6e61af50d068aa96d8d4773855ce5700772c2f
ChaeAg <109871579+ChaeAg@users.noreply.github.com>
2026-06-20 10:49:03 +0900

    [Medium] Title: 0075-sort-colors, Time: 2 ms (16.85%), Memory: 43.3 MB (75.66%) - LeetHub

--- LeetCode/0075-sort-colors/0075-sort-colors.java
+++ LeetCode/0075-sort-colors/0075-sort-colors.java
@@ -1,19 +1,14 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int c0 = 0, c1 = 0, c2 = 0;
-        for(int num : nums) {
-            if(num == 0) c0++;
-            else if(num == 1) c1++;
-            else c2++;
-        }
-
-        for(int i=0; i<nums.length; i++) {
-            if(c0-- > 0) {
-                nums[i] = 0;
-            } else if(c1-- > 0) {
-                nums[i] = 1;
-            } else {
-                nums[i] = 2;
+        for(int i=1; i<nums.length; i++) {
+            int ic = i;
+            for(int j=i-1; j>=0; j--) {
+                if(nums[ic] < nums[j]) {
+                    int tmp = nums[ic];
+                    nums[ic] = nums[j];
+                    nums[j] = tmp;
+                    ic = j;
+                }
             }
         }
     }
